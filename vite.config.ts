d4b076503e392945ecbe3292770ee96d12897afc
ckvv <k_0520@qq.com>
2024-11-06 21:11:04 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
-        entry: 'electron/main.ts',
+        entry: 'src/electron/main.ts',
         vite: {
           build: {
             target: 'esnext',
@@ -44,7 +44,7 @@ export default defineConfig({
       preload: {
         // Shortcut of `build.rollupOptions.input`.
         // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'electron/preload.ts'),
+        input: path.join(__dirname, 'src/electron/preload.ts'),
       },
       // Ployfill the Electron and Node.js API for Renderer process.
       // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
